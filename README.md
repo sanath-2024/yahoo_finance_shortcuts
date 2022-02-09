c007c81ddbcb888b7ae7ab69c33d6473e8c42a7c
Sanath Govindarajan <sgovindarajan@utexas.edu>
2022-02-09 11:50:30 -0600

    initial commit - working MVP

---
+++ README.md
@@ -0,0 +1,14 @@
+# Yahoo Finance Shortcuts
+
+I made a Chrome extension to help investors quickly get to the page they want on Yahoo Finance.
+
+This is an *unofficial* extension not supported in any way by Yahoo.
+
+## Installation Instructions
+
+1. Download the repository as a zip.
+2. Unzip the repository.
+3. Navigate to `chrome://extensions` in the Google Chrome browser.
+4. If developer mode is not already turned on, turn it on by pressing the slider at the upper right-hand corner of the page.
+5. Click the "Load unpacked" button.
+6. Point Google Chrome to the directory (folder) that you unzipped in step 2.
---
+++ popup.js
@@ -0,0 +1,20 @@
+getTicker = () => document.getElementById('ticker').value;
+createTab = (baseUrl, ticker, subUrl) => {
+    chrome.tabs.create({url: `${baseUrl}/${ticker}/${subUrl}`})
+};
+base = 'https://finance.yahoo.com/quote';
+subUrls = [
+    'key-statistics',
+    'history',
+    'profile',
+    'financials',
+    'balance-sheet',
+    'cash-flow',
+    'analysis',
+    'options'
+];
+document.getElementById('quote').onclick = () => createTab(base, getTicker(), '');
+for (const subUrl of subUrls) {
+    document.getElementById(subUrl).onclick = () => createTab(base, getTicker(), subUrl);
+}
+document.getElementById('ticker').focus();
